babbd1f5d9b9671829138e71529e221fcd35501a
Cerosh <ceroshjacob@gmail.com>
2021-02-27 22:08:23 +1100

    * Program.cs: updated
    * TypesInCSharp.csproj:
    
    * GitHubVSCSharpLearning.sln: Corrected project name

--- first_application/Program.cs
+++ first_application/Program.cs
@@ -7,6 +7,7 @@ namespace first_application
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Checked");
         }
     }
 }
