d37ddf135fea19e92c985bd35d230d0be84239bc
Cerosh <ceroshjacob@gmail.com>
2021-02-27 22:44:24 +1100

    * Types.cs: * TypesInCSharp.csproj: Updated data types in CSharp
    * Program.cs: Removing this file from the solution.

---
+++ first_application/Types.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace TypesInCSharp
+{
+    public class EmptyClass
+    {
+       
+
+
+        static void Main(string[] args)
+        {
+            int myNum = 5;
+            Console.WriteLine( $"Integer (whole number): {myNum}");
+
+            double myDoubleNum = 5.99D;
+            Console.WriteLine($"Floating point number: {myDoubleNum}");
+
+            char myLetter = 'D';
+            Console.WriteLine($"Character: {myLetter}");
+
+            bool myBool = true;
+            Console.WriteLine($"Boolean : {myBool}");
+            string myText = "Hello";  
+            Console.WriteLine($"String :{myText}");
+            Console.Read();
+        }
+    }
+}
