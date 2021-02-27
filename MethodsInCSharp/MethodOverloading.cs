01af6a9da8adf0f45547ec3afec266bbd6841b16
Cerosh <ceroshjacob@gmail.com>
2021-02-28 08:44:45 +1100

    * MethodsInCSharp.csproj: created a project for Methods in CSharp
    * MethodOverloading.cs: Class for method overload
    
    * Program.cs: main class to acess

---
+++ MethodsInCSharp/MethodOverloading.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MethodsInCSharp
+{
+    public class MethodOverloading
+    {
+        public void Add()
+        {
+            Console.WriteLine("This is a empty argument method");
+        }
+
+        public void Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine("The sum of arguments is: "+ result);
+        }
+    }
+}
---
+++ MethodsInCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MethodsInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodOverloading mo = new MethodOverloading();
+            mo.Add();
+            mo.Add(4, 5);
+            Console.Read();
+        }
+    }
+}
