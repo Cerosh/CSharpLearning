bab66b37c3f5a33a837ccf2195784ee2564e3148
Cerosh <ceroshjacob@gmail.com>
2021-02-28 23:09:42 +1100

    * Loops.cs: included examples of various types of loops like while,   do-while, for, foreach and break , continue statmemnets.
    * LoopsInCSharp.csproj: Created a project for loops
    
    * Program.cs: Launching page for Loop page.
    
    * GitHubVSCSharpLearning.sln: Created solution folder for Loops

---
+++ LoopsInCSharp/Loops.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace LoopsInCSharp
+{
+    public class Loops
+    {
+        public void whileLoop(int var1)
+        {
+            Console.WriteLine("Inside While Loop Method ");
+            while (var1 < 5)
+            {
+                Console.WriteLine(var1);
+                var1++;
+            }
+        }
+        public void doWhileLoop(int var1)
+        {
+            Console.WriteLine("Inside Do While Loop Method ");
+            do
+            {
+                Console.WriteLine(var1);
+                var1++;
+            } while (var1 < 5);
+        }
+        public void forLoop(int max)
+        {
+            Console.WriteLine("Inside For Loop Method ");
+            for (int i = 0; i < max; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void forEachLoop()
+        {
+            Console.WriteLine("Inside For Each Loop Method ");
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach (string item in cars)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void breakStatmentInLoop()
+        {
+            Console.WriteLine("Inside For Break statement Method ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+
+        public void continueStatementInLoop()
+        {
+            Console.WriteLine("Inside For Contniue Statement Method ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ LoopsInCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LoopsInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loops loop = new Loops();
+            loop.whileLoop(6);
+            loop.doWhileLoop(6);
+            loop.forLoop(5);
+            loop.forEachLoop();
+            loop.breakStatmentInLoop();
+            loop.continueStatementInLoop();
+        }
+    }
+}
