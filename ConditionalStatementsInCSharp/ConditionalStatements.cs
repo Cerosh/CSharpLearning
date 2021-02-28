349b2dde5cb3c394d48686627bd4d1a666f877fc
Cerosh <ceroshjacob@gmail.com>
2021-02-28 17:49:09 +1100

    * ConditionalStatements.cs: create a class with methods for   conditional statements
    * ConditionalStatementsInCSharp.csproj: created a project for
      conditional statements
    
    * Program.cs: triggering class for conditional statmeents
    
    * GitHubVSCSharpLearning.sln: created a solution folder for
      condiutional statements

---
+++ ConditionalStatementsInCSharp/ConditionalStatements.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace ConditionalStatementsInCSharp
+{
+    public class ConditionalStatements
+    {
+        public void ifStatment(int val1, int val2)
+        {
+            if (val1 > val2)
+            {
+                Console.WriteLine($"{val1} is greater than {val2}");
+            }
+        }
+
+        public void ifElseStatement(int val1, int val2)
+        {
+            if (val1 > val2)
+            {
+                Console.WriteLine($"{val1} is greater than {val2}");
+            }
+            else
+            {
+                Console.WriteLine($"{val1} is Less than {val2}");
+            }
+        }
+
+        public void ternaryOperator(int time)
+        {
+            String result = (time < 18) ? "Good day" : "Good evening";
+            Console.WriteLine( result);
+        }
+        public void switchCase(int dayOfWeek)
+        {
+            switch (dayOfWeek)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                default:
+                    Console.WriteLine("Not Defined day");
+                    break;
+            }
+        }
+    }
+}
---
+++ ConditionalStatementsInCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConditionalStatementsInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConditionalStatements cs = new ConditionalStatements();
+            cs.ifStatment(20, 15);
+            cs.ifElseStatement(15, 20);
+            cs.ternaryOperator(15);
+            cs.switchCase(2);
+        }
+    }
+}
