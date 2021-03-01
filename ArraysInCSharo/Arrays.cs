81aad901ec4f9996916784bcb6435c1635251349
Cerosh <ceroshjacob@gmail.com>
2021-03-01 20:28:37 +1100

    * Arrays.cs: Included new Array methods
    * ArraysInCSharp.csproj: created a project for Arrays
    
    * Program.cs: Program for Array methods
    
    * GitHubVSCSharpLearning.sln: Created solution for Array

---
+++ ArraysInCSharo/Arrays.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace ArraysInCSharp
+{
+    public class Arrays
+    {
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        int[] myNumbers = { 5, 1, 8, 9 };
+
+        public void createArray()
+        {
+            String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void accessingArray()
+        {
+            int[] scores = { 100, 123, 151, 171 };
+            Console.WriteLine(scores[0]);
+        }
+        public void changeArrayElement(String carName)
+        {
+            cars[3] = carName;
+            Console.WriteLine(cars[3]);
+        }
+        public void arrayLength()
+        {
+            Console.WriteLine(cars.Length);
+        }
+        public void sortArray()
+        {
+            
+            Array.Sort(myNumbers);
+            foreach (var item in myNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void otherArrayMethods()
+        {
+            Console.WriteLine("Max: " + myNumbers.Max());
+            
+            Console.WriteLine("Min: " + myNumbers.Min());
+           
+            Console.WriteLine("Sum: " + myNumbers.Sum());
+            
+
+        }
+    }
+}
---
+++ ArraysInCSharo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ArraysInCSharp;
+
+namespace ArraysInCSharo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arrays array = new Arrays();
+            Console.WriteLine("Create Array");
+            array.createArray();
+
+            Console.WriteLine("Accessing Array");
+            array.accessingArray();
+
+            Console.WriteLine("Array Length");
+            array.arrayLength();
+
+            Console.WriteLine("Change Arrray element");
+            array.changeArrayElement("Opel");
+
+            Console.WriteLine("Min, Max and Sum");
+            array.otherArrayMethods();
+
+            Console.WriteLine("Sort Array");
+            array.sortArray();
+        }
+    }
+}
